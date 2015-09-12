d571d6dd22928bd897addaf23cf4e35b8eeda844
markovAntony <abbrown22@gmail.com>
2015-09-12 12:46:35 -0500

    Changed settings.py

--- injustice/settings.py
+++ injustice/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'injustice.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 import dj_database_url
-DATABASES['default'] = dj_database_url.config()
+DATABASES = { 'default': dj_database_url.config() }
 """
 DATABASES = {
     'default': {

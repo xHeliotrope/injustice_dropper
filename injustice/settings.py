260a943f40c2b6f7a18d01f2f5893e5833871064
markovAntony <abbrown22@gmail.com>
2015-09-12 13:40:22 -0500

    Added sys.path

--- injustice/settings.py
+++ injustice/settings.py
@@ -15,6 +15,9 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+import sys
+sys.path.append('injustice')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -87,7 +90,6 @@ DATABASES = {
     }
 }
 """
-
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
